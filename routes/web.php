734335604a1cd3f162db9dada985e74b6330606f
Jesus <jesuschilca@gmail.com>
2018-06-05 16:27:45 -0500

    proyecto funcionando

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -8,24 +7,16 @@
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-//|
-//*/
-//
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-
-
+|
+*/
 Route::get('/', 'MainController@index');
 Route::get('/Administrador', function () {
     return view('auth/login');
 });
-// rutas para los eventos\
-
+// rutas para los eventos
 Route::bind('evento', function($id){
  return SisBezaFest\Paquete::where('id', $id)->first(); 
 });
-
 //rutas del partner
 Route::resource('partner/usuario','PersonaController');
 Route::resource('partner/clientes','ClienteController');
@@ -48,16 +39,12 @@ Route::get('administrador/empresa/buscarPersona','EmpresaController@buscarperson
 //rutas carrito de compras
 Route::get('main/shoppincar', 'ShoppingCartsController@index');
 Route::get('main/shoppincar/{id}', 'InShoppingCartsController@strore')->name('shopping');
-
-
 Route::resource('administrador/empresa','EmpresaController');
-
 Auth::routes();
 //Route::resource('','HomeController');
 //Route::get('/home', 'HomeController@index')->name('home');
 //Route::get('/{str_slug($url)?}', 'HomeController@index');
 //Route::get('/home', 'AdminController@index')->name('welcome'
-
 //carrito
 Route::get('main/cart',['as' => 'cart-show','uses'=>'CartController@show']);
 Route::get('main/add/{evento}',['as' => 'cart-add','uses'=>'CartController@add']);
